fb9773e6c0e8ec015418bcf5003e26741fe6d6a4
rizkaniahrtp <rizkaniahartikaputri@upi.edu>
2026-01-04 11:09:55 +0700

    UAS PROMNET - RIZKANIA HARTIKA PUTRI_2402847_3A

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -46,52 +46,7 @@
                             </div>
                         </li> -->
 
-                        <!-- Nav Item - Alerts -->
-                        <li class="nav-item dropdown no-arrow mx-1">
-
-                            <!-- Dropdown - Alerts -->
-                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="alertsDropdown">
-                                <h6 class="dropdown-header">
-                                    Alerts Center
-                                </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-warning">
-                                            <i class="fas fa-file-alt text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 12, 2019</div>
-                                        <span class="font-weight-bold">A new monthly report is ready to download!</span>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-success">
-                                            <i class="fas fa-donate text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 7, 2019</div>
-                                        $290.29 has been deposited into your account!
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-warning">
-                                            <i class="fas fa-exclamation-triangle text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 2, 2019</div>
-                                        Spending Alert: We've noticed unusually high spending for your account.
-                                    </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-                            </div>
-                        </li>
-
+                        
                         <!-- Nav Item - Messages -->
                         <li class="nav-item dropdown no-arrow mx-1">
                             <a class="nav-link dropdown-toggle" href="/inbox" id="messagesDropdown" role="button"
@@ -155,11 +110,11 @@
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in bg-warning"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="/profil">
+                                <a class="dropdown-item" href="/admin/profil">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-white-400"></i>
                                     Profil
                                 </a>
-                                <a class="dropdown-item" href="/ubah_password">
+                                <a class="dropdown-item" href="/admin/ubah_password">
                                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-white-400"></i>
                                     Ubah Password
                                 </a>
